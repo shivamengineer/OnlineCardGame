14caa7f28b853ec4c4c40fd64286d26c69a9c4b8
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-04-13 00:05:17 -0400

    can now run across windows

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.use(express.static('public'))
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000});
+
+const port = 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const players = {};
+players.numPlayers = 0;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  const numPlayers = players.numPlayers;
+  players[socket.id] = {
+    x: 100,
+    y: 100,
+    index: numPlayers
+  };
+  players.numPlayers++;
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+    delete players[socket.id];
+    players.numPlayers--;
+    io.emit('updatePlayers', players);
+  });
+
+  //socket.emit for local, io.emit for everyone
+  io.emit('updatePlayers', players);
+
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,39 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const socket = io();
+
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const x = canvas.width / 2;
-const y = canvas.height / 2;
+const players = {};
 
-const player = new Player(x, y, 40, 40, 'white');
+socket.on('updatePlayers', (backendPlayers) => {
+  for(const id in backendPlayers){
+    const backendPlayer = backendPlayers[id];
+    if(!players[id]){
+      players[id] = new Player(backendPlayer.x + (50 * backendPlayer.index), backendPlayer.y, 30, 30, "white");
+    }
+  }
+  
+  for(const id in players){
+    if(!backendPlayers[id]){
+      delete players[id];
+    }
+  }
+
+  console.log(players);
+
+  draw();
+});
 
 function draw() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  player.render();
-}
+  for(const id in players){
+    const newPlayer = players[id];
+    newPlayer.render();
+  }
 
-draw();
+}
