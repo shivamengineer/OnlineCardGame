091def9aecc6b484b8a895efc5785b5a1d029841
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-05-14 22:06:57 -0400

    added basic user input
    gets mouse position and prints only on client side

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -1,3 +1,6 @@
-/*addEventListener('keydown', (event) => {
-  
-});*/
+addEventListener('mousedown', (event) => {
+    if(!players[socket.id]) return;
+
+    console.log("x: " + event.clientX);
+    console.log("y: " + event.clientY);
+});
