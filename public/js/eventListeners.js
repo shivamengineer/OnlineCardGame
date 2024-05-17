9750a449904e47ac5491d8ffc3770182d353e26b
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-05-16 23:37:56 -0400

    allows user to drag and drop cards

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -6,7 +6,10 @@ addEventListener('mousedown', (event) => {
     var card = -1;
     for(const i in cards){
         if(mouseCollidesCard(mouseX, mouseY, cards[i], img)){
-            cards[i].rotation++;
+            //cards[i].rotation++;
+            cards[i].moving = true;
+            cards[i].differenceX = mouseX - cards[i].x;
+            cards[i].differenceY = mouseY - cards[i].y;
         }
     }
 
@@ -16,10 +19,31 @@ addEventListener('mousedown', (event) => {
 addEventListener('mousemove', (event) => {
     if(!players[socket.id] ) return;
 
+    for(const i in cards){
+        if(cards[i].moving){
+            var mouseX = event.clientX - cards[i].differenceX;
+            var mouseY = event.clientY - cards[i].differenceY;
+            cards[i].x = mouseX;
+            cards[i].y = mouseY;
+        }
+    }
+
+    draw();
 });
 
 addEventListener('mouseup', (event) => {
     if(!players[socket.id]) return;
 
+    for(const i in cards){
+        if(cards[i].moving){
+            var mouseX = event.clientX - cards[i].differenceX;
+            var mouseY = event.clientY - cards[i].differenceY;
+            cards[i].x = mouseX;
+            cards[i].y = mouseY;
+            cards[i].moving = false;
+        }
+    }
+
+    draw();
 
 });
