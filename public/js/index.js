e5cd2c9fa57d06fc05bac4d44bcf37fdcacceb8e
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-05-28 03:08:41 -0400

    created button that allows the user to switch between homescreen and playing the game

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,8 @@ var cardMenuOpen = false;
 var shift = false;
 var numPlayers = 0;
 var cardSelected = -1;
+var homescreen = false;
+var homeScreenRect = new Rect(300, 300, 200, 50, "white");
 
 for(i = 0; i < 2; i++){
   menuOptions[i] = new Rect(0, 0, menuAttribs[2], menuAttribs[3], menuAttribs[4]);
