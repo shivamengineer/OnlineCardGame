2149f46306503f91c69850ee2aff88a08eb26fb4
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-05-29 02:20:48 -0400

    added text attribute to home screen button

--- public/js/index.js
+++ public/js/index.js
@@ -20,6 +20,8 @@ var numPlayers = 0;
 var cardSelected = -1;
 var homescreen = false;
 var homeScreenRect = new Rect(300, 300, 200, 50, "white");
+homeScreenRect.text = "Home Screen";
+
 
 for(i = 0; i < 2; i++){
   menuOptions[i] = new Rect(0, 0, menuAttribs[2], menuAttribs[3], menuAttribs[4]);
