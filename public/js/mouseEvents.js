b944083f97c42c5de6059cda207b0f842a70426a
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-06-02 02:03:44 -0400

    correctly updates text on button rects to then switch to different pages easily

--- public/js/mouseEvents.js
+++ public/js/mouseEvents.js
@@ -18,45 +18,58 @@ function mouseDownEventGame(e){
     draw();
 }
 
-function mouseDownEventHomescreen(e){
+function mouseDownEventButtons(e){
     var mouseX = e.clientX;
     var mouseY = e.clientY;
-    if(mouseCollides(mouseX, mouseY, homeScreenRect)){
-        switch(currentPage){}
-        if(currentPage == 0){
-            currentPage = 1;
-            homeScreenRect.text = "Home Screen";
-        } else {
-            currentPage = 0;
-            homeScreenRect.text = "Game";
+    if(mouseCollides(mouseX, mouseY, button1Rect)){
+        switch(currentPage){
+            case 0:
+                currentPage = 1;
+                button1Rect.text = "Game";
+                break;
+            case 1:
+                currentPage = 0;
+                button1Rect.text = "Home Screen";
+                break;
+            case 2:
+                currentPage = 0;
+                button1Rect.text = "Home Screen";
+                button2Rect.text = "Rules Engine";
+                break;
+        }
+        draw();
+    } else if(mouseCollides(mouseX, mouseY, button2Rect)){
+        switch(currentPage){
+            case 0:
+                currentPage = 2;
+                button1Rect.text = "Game";
+                button2Rect.text = "Home Screen";
+                break;
+            case 1:
+                currentPage = 2;
+                button2Rect.text = "Home Screen";
+                break;
+            case 2:
+                currentPage = 1;
+                button2Rect.text = "Rules Engine";
+                break;
         }
         draw();
     }
 }
 
-function mouseDownEventRulesEngine(e){
-    var mouseX = e.clientX;
-    var mouseY = e.clientY;
-    if(mouseCollides(mouseX, mouseY, rulesEngineRect)){
-        currentPage = 2;
-        homeScreenRect.text = "Home Screen";
-    }
-    draw();
-}
-
 function mouseDownAlways(e){
-    mouseDownEventHomescreen(e);
-    mouseDownEventRulesEngine(e);
+    mouseDownEventButtons(e);
 }
 
 function mouseMoveEvent(e){
-    if(!homescreen){
+    if(currentPage == 0){
         mouseMoveEventGame(e);
     }
 }
 
 function mouseUpEvent(e){
-    if(!homescreen){
+    if(currentPage == 0){
         mouseUpEventGame(e);
     }
 }
