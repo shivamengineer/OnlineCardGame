f050f8a6a1da4cac0da77d5ac8734ca9ae79689b
SophiaRak <sophiarakowsky@gmail.com>
2024-06-06 12:03:53 -0400

    New Card Size (new cards)

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const img = document.getElementById("AceOfSpades");
+const img = document.getElementById("10S");
 var ctx2 = canvas.getContext('2d');
 
 const socket = io();
