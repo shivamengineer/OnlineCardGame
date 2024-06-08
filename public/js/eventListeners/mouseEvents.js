d9019ff6a19f0d551b55d0ce9466e96462fbf1f6
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-06-08 01:25:32 -0400

    moved event listener files to folder

---
+++ public/js/eventListeners/mouseEvents.js
@@ -0,0 +1,24 @@
+function mouseDownEvent(e){
+    mouseDownAlways(e);
+    if(currentPage == 0){
+        mouseDownEventGame(e);
+    } else if(currentPage == 2){
+        mouseDownMoveBlock(e.clientX, e.clientY);
+    }
+}
+
+function mouseMoveEvent(e){
+    if(currentPage == 0){
+        mouseMoveEventGame(e);
+    } else if(currentPage == 2){
+        mouseMoveRulesBlock(e);
+    }
+}
+
+function mouseUpEvent(e){
+    if(currentPage == 0){
+        mouseUpEventGame(e);
+    } else if(currentPage == 2){
+        mouseUpRulesBlock(e);
+    }
+}
