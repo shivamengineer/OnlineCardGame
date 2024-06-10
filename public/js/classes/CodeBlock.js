65e6b20eb19277fff5f0a9228ec11c8156ff0b0d
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-06-10 01:00:43 -0400

    fixed bug where blocks could cover other blocks when two blocks were combined

--- public/js/classes/CodeBlock.js
+++ public/js/classes/CodeBlock.js
@@ -13,13 +13,15 @@ class CodeBlock {
                     tempBlocks.push(this.blocks.pop());
                 }
                 while(block1.blocks != -null){
-                    block1.blocks[0].x = this.blocks[0].x;
-                    block1.blocks[0].y = this.blocks[0].y + (this.height * this.blocks.length);
+                    //block1.blocks[0].x = this.blocks[0].x;
+                    //block1.blocks[0].y = this.blocks[0].y + (this.height * this.blocks.length);
                     this.blocks.push(block1.blocks.shift());
                 }
                 while(tempBlocks != -null){
+                    //tempBlocks[tempBlocks.length - 1].y = this.blocks[0].y + (this.height + this.blocks.length);
                     this.blocks.push(tempBlocks.pop()); 
                 }
+                this.moveWholeBlock(this.blocks[0].x, this.blocks[0].y);
             }
         }
         return added;
